9ec386f15be8df66fd33818a5f2d73d297abed36
Jalen Adams <leftysolara@gmail.com>
2017-12-16 10:48:21 -0500

    set up compilation with ncurses

---
+++ src/ui.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * ui.h : user-facing elements of the program
+ * ****************************************************************************
+ * Copyright (C) 2017 Jalen Adams
+ *
+ * Authors: Jalen Adams <leftysolara@gmail.com>
+ *
+ * This file is part of Pantomime.
+ *
+ * Pantomime is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pantomime is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pantomime.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
+#ifndef UI_H
+#define UI_H
+
+#include <ncurses.h>
+#include <panel.h>
+
+#endif
