26a088a1223d3c65323ff672803d4f2d4d9947ad
Jalen Adams <jalen@jalenkadams.me>
2019-09-28 11:49:02 -0400

    Get ncurses linked into project

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * main.c
+ * ui.h
  *******************************************************************************
  * Copyright (C) 2019  Jalen Adams
  *
@@ -7,21 +7,31 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-#include "ui/ui.h"
+#include "ui.h"
 
-int main()
+#include <ncurses.h>
+#include <locale.h>
+
+void start_curses()
 {
-    start_curses();
-    end_curses();
-    return 0;
+    setlocale(LC_ALL, "");
+    initscr();
+    printw("Hello World!");
+    refresh();
+    getch();
+}
+
+void end_curses()
+{
+    endwin();
 }
--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * main.c
+ * ui.h
  *******************************************************************************
  * Copyright (C) 2019  Jalen Adams
  *
@@ -7,21 +7,20 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-#include "ui/ui.h"
+#ifndef UI_H
+#define UI_H
 
-int main()
-{
-    start_curses();
-    end_curses();
-    return 0;
-}
+void start_curses();
+void end_curses();
+
+#endif /* UI_H */
